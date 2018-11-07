bd096390d824105016179360f8a12cfa2275f963
root <root@db9e5a361cc1>
2018-11-07 08:10:40 +0000

    [AOM] Change code for module SalesModule2 in system LIVE

---
+++ src/com/apiomat/nativemodule/salesmodule2/ContractProtocol.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2011 - 2018, Apinauten GmbH
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.apiomat.nativemodule.salesmodule2;
+
+
+import com.apiomat.nativemodule.basics.*;
+/**
+* Generated class for your ContractProtocol data model
+*
+* DO NOT CHANGE ANY CODE EXCEPT CLASS ANNOTATIONS OR CLASS ATTRIBUTES HERE!
+* EVERYTHING ELSE WILL GET OVERWRITTEN!
+*
+*/
+@java.lang.SuppressWarnings( "unused" )
+@com.apiomat.nativemodule.Model( moduleName = "SalesModule2",
+    hooksClassNameTransient = "com.apiomat.nativemodule.salesmodule2.ContractProtocolHooksTransient", 
+    hooksClassNameNonTransient = "com.apiomat.nativemodule.salesmodule2.ContractProtocolHooksNonTransient", 
+    isTransient = false,     requiredUserRoleCreate=com.apiomat.nativemodule.UserRole.User, requiredUserRoleRead=com.apiomat.nativemodule.UserRole.User,
+    requiredUserRoleWrite=com.apiomat.nativemodule.UserRole.Owner, restrictResourceAccess=false,    allowedRolesCreate={}, allowedRolesRead={},
+    allowedRolesWrite={}, allowedRolesGrant={}, 
+    roleClassesMap={})
+public class ContractProtocol extends com.apiomat.nativemodule.AbstractClientDataModel<com.apiomat.nativemodule.salesmodule2.ContractProtocol> implements com.apiomat.nativemodule.IModel<com.apiomat.nativemodule.salesmodule2.ContractProtocol>
+{
+    /**
+     * Contains the name of the module that this model belongs to
+     */
+    public static final String MODULE_NAME = "SalesModule2";
+    /**
+     * Contains the name of the model
+     */
+    public static final String MODEL_NAME = "ContractProtocol";
+
+    /** class specific attributes */
+    private String notes = null;
+    private java.util.Date timeOfContact = null;
+    /**
+     * Protected constructor; to create a new instance, use the createObject() method
+     */
+    public ContractProtocol ()
+    {}
+
+    /**
+     * Returns the name of the module where this class belongs to
+     */
+    @Override
+    public String getModuleName( )
+    {
+        return MODULE_NAME;
+    }
+
+    /**
+     * Returns the name of the model
+     */
+    @Override
+    public String getModelName( )
+    {
+        return MODEL_NAME;
+    }
+
+    public String getNotes()
+    {
+         return this.notes;
+    }
+
+    public void setNotes( String arg )
+    {
+        this.notes = arg;
+    }
+
+    public java.util.Date getTimeOfContact()
+    {
+         return this.timeOfContact;
+    }
+
+    public void setTimeOfContact( java.util.Date arg )
+    {
+        this.timeOfContact = arg;
+    }
+
+}
